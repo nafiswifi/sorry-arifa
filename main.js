d650c998fb5fdbe33315dc660a2ab2b43274092f
nafiswifi <144604929+nafiswifi@users.noreply.github.com>
2025-09-20 19:37:44 +0600

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-
-var el =  document.getElementById('ptag').textContent = setInterval(function(){document.writeln('<div style="margin: 10px"><span style="color: blueviolet; font-size: 18px;background: white; box-shadow: 0px 0px 8px -1px blueviolet;  padding: 7.3px 14px; border-radius: 12.5px; letter-spacing: 1.1px;border: 1px solid blueviolet;"> Sorry,  arifa  </span> </div> <br>');
-  }, 400);
-  
