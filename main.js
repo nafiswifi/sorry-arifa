671393ee7455214932f4053ea0327641b3f86c09
nafiswifi <144604929+nafiswifi@users.noreply.github.com>
2025-09-20 19:55:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+
+var el =  document.getElementById('ptag').textContent = setInterval(function(){document.writeln('<div style="margin: 10px"><span style="color: blueviolet; font-size: 18px;background: white; box-shadow: 0px 0px 8px -1px blueviolet;  padding: 7.3px 14px; border-radius: 12.5px; letter-spacing: 1.1px;border: 1px solid blueviolet;"> Sorry,  arifa  </span> </div> <br>');
+  }, 400);
+  
